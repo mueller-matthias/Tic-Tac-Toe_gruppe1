5b67d90bec4d593aeed12e4632010a44a1d9692d
Matthias Müller <matthias.mueller@ksb-sg.ch>
2025-03-17 08:51:54 +0100

    varaibles and functions added

--- Tic-Tac-Toe_gruppe1/GameBoardModel.cs
+++ Tic-Tac-Toe_gruppe1/GameBoardModel.cs
@@ -9,25 +9,89 @@ namespace Tic_Tac_Toe_gruppe1
     public class GameBoardModel
     {
         private char[,] board;
+        private int groesse;
 
-        public GameBoardModel(int size) {
+        public GameBoardModel(int size)
+        {
+            groesse = size;
             board = new char[size, size];
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++)
+            {
                 for (int j = 0; j < size; j++)
                 {
                     board[i, j] = ' ';
                 }
-
             }
         }
 
-        public char GetCell(int row, int col) { 
+        public char GetCell(int row, int col)
+        {
             return board[row, col];
         }
 
         public void SetCell(int row, int col, char symbol)
         {
             board[row, col] = symbol;
+        }
+
+        public bool ValidateMove(Spieler spieler, int row)
+        {
+            return row >= 0 && row < groesse && board[row, 0] == ' ';
+        }
+
+        public bool pruefeGewinner(GameBoardModel spielfeld, Spieler spieler)
+        {
+            char symbol = spieler.Symbol;
+
+            // Horizontale und vertikale Prüfung
+            for (int i = 0; i < groesse; i++)
+            {
+                if (CheckRow(i, symbol) || CheckColumn(i, symbol))
+                    return true;
+            }
+
+            // Diagonale Prüfung
+            return CheckDiagonal(symbol) || CheckAntiDiagonal(symbol);
+        }
+
+        private bool CheckRow(int row, char symbol)
+        {
+            for (int i = 0; i < groesse; i++)
+            {
+                if (board[row, i] != symbol)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckColumn(int col, char symbol)
+        {
+            for (int i = 0; i < groesse; i++)
+            {
+                if (board[i, col] != symbol)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckDiagonal(char symbol)
+        {
+            for (int i = 0; i < groesse; i++)
+            {
+                if (board[i, i] != symbol)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckAntiDiagonal(char symbol)
+        {
+            for (int i = 0; i < groesse; i++)
+            {
+                if (board[i, groesse - 1 - i] != symbol)
+                    return false;
+            }
+            return true;
         }
     }
 }
--- Tic-Tac-Toe_gruppe1/GameController.cs
+++ Tic-Tac-Toe_gruppe1/GameController.cs
@@ -9,5 +9,21 @@ namespace Tic_Tac_Toe_gruppe1
     internal class GameController
     {
 
+        private Spieler aktuellerSpieler;
+        private int spielmodus;
+        private GameBoardModel spielfeld;
+        private Timer timer;
+        private WinChecker winChecker;
+        private UndoFunction undo;
+        private Protokoll protokoll;
+
+        public void starten() { }
+        public void beenden() { }
+        public void rückgängigMachen() { }
+        public bool validiereZug(Spieler spieler, int position) { }
+        public bool pruefeGewinn() { }
+
+
+
     }
 }
